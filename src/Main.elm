9d72cc3ff59f18ef450532708fb7b6c0c4340723
Pablo Bollansée <pablo.bollansee@gmail.com>
2022-12-10 15:59:53 +0000

    Tryout for selecting and capturing ripples

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,30 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, text)
+import File exposing (File)
+import Html exposing (Html, div, img, input, label, text)
+import Html.Attributes exposing (accept, attribute, hidden, src, style, type_)
+import Html.Events exposing (on)
+import Json.Decode as D
+import Task
 
 
-type Model
-    = NothingYet
+type alias ImagePreviewUrl =
+    String
+
+
+type alias LocalRippleImage =
+    { image : File, preview : ImagePreviewUrl }
+
+
+type alias Model =
+    { localRipple : Maybe LocalRippleImage
+    }
 
 
 type Msg
-    = DoNothing
+    = SetLocalRippleImage File
+    | SetLocalRippleImagePreview File ImagePreviewUrl
 
 
 main : Program () Model Msg
@@ -24,7 +39,7 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( NothingYet
+    ( { localRipple = Nothing }
     , Cmd.none
     )
 
@@ -36,12 +51,75 @@ subscriptions _ =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SetLocalRippleImage image ->
+            ( model
+            , Task.perform (SetLocalRippleImagePreview image) (File.toUrl image)
+            )
+
+        SetLocalRippleImagePreview image preview ->
+            ( { model | localRipple = Just { image = image, preview = preview } }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
 view model =
     div
         []
-        [ text "Hello"
+        [ text "Hello test"
+        , viewMakeRippleButton
+            Select
+            SetLocalRippleImage
+            [ div [ style "font-size" "100px" ] [ text "📂" ]
+            ]
+        , viewMakeRippleButton
+            Capture
+            SetLocalRippleImage
+            [ div [ style "font-size" "100px" ] [ text "📸" ]
+            ]
+        , case model.localRipple of
+            Nothing ->
+                div []
+                    [ text "No ripple selected"
+                    ]
+
+            Just image ->
+                img [ src image.preview ]
+                    []
         ]
+
+
+type RippleMethod
+    = Select
+    | Capture
+
+
+viewMakeRippleButton : RippleMethod -> (File -> msg) -> List (Html msg) -> Html msg
+viewMakeRippleButton rippleMethod onChange innerHtml =
+    label [] <|
+        let
+            baseAttrs =
+                [ type_ "file"
+                , accept "image/*"
+                , on "change" <| D.map onChange inputFileDecoder
+                , hidden True
+                ]
+        in
+        input
+            (case rippleMethod of
+                Select ->
+                    -- Opening a selection dialog is the default, so add nothing
+                    baseAttrs
+
+                Capture ->
+                    -- To capture directly from the camera add the "capture" attribute
+                    attribute "capture" "environment" :: baseAttrs
+            )
+            []
+            :: innerHtml
+
+
+inputFileDecoder : D.Decoder File
+inputFileDecoder =
+    D.at [ "target", "files" ] (D.oneOrMore (\first _ -> first) File.decoder)
